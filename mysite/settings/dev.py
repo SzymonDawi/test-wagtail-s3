c19daf5fc67790b646620cff27d45718c05730ac
Szymon <szymdawi@gmail.com>
2024-05-19 21:37:47 +1200

    Init repo

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-h%e55w$d5%ye*q)ehs0u(_kp8ihm!3z6r&+dv0&_64-mtkt@*&"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
